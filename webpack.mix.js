b49da246e6247f8c344cfe0dbade9b38b8ad0ac0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-02-16 09:02:33 +0100

    webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js([
+    'resources/js/app.js',
+], 'public/js/app.js').version()
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/start.scss', 'public/css');
+
 mix.browserSync('127.0.0.1:8000');
