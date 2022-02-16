42f3359e860687759d6ac36f3d98656d1f9cd480
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-02-16 12:23:46 +0100

    email updates

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -2,106 +2,80 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Email</title>
     </head>
-    <link rel="stylesheet" href="css/app.css" />
-    <style>
-        body {
+
+    <body
+        style="
             text-align: center;
             font-family: Cormorant;
-        }
-        h1 {
-            font-size: 56px;
-        }
-        h2 {
-            font-size: 30px;
-        }
-        h3 {
-            font-size: 12px;
-        }
-        h4 {
-            font-size: 26px;
-            font-family: Poppins;
-            font-weight: 700;
-            color: #f8f3f1;
-        }
-        .email-container {
-            width: 1000px;
-            padding: 50px;
             background-color: #f8f3f1;
-            margin: auto;
-        }
-        .email-footer {
-            background-color: #36393b;
-            color: #f8f3f1;
-            font-family: Poppins;
-            padding: 25px;
-        }
-        .img-fluid {
-            width: clamp(250px, 50%, 500px);
-        }
-        .img-building {
-            width: clamp(500px, 100%, 900px);
-        }
-        .text-left {
-            text-align: left;
-        }
-
-        .font-secondary {
-            font-family: Poppins;
-            font-weight: 300;
-            font-size: 16px;
-        }
-        .img-building2 {
-            width: clamp(230px, 95%, 280px);
-        }
-        .container-center {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        .card {
-            display: grid;
-            align-content: center;
-        }
-        a {
-            text-decoration: underline;
-        }
-    </style>
-    <body>
-        <div class="email-container">
-            <div class="card">
+        "
+    >
+        <div
+            class="email-container"
+            style="
+                width: 300px;
+                max-width: 500px;
+                padding: 50px;
+                background-color: #f8f3f1;
+                margin: auto;
+            "
+        >
+            <div class="card" style="display: grid; align-content: center">
                 <div class="email-header">
                     <img
-                        src="https://bielanypark.pl/images/logos/Logo_grey.svg"
+                        src="http://drive.google.com/uc?export=view&id=1AUXt2DiEMwAPUvTB8TrkLY1JWZzIvvdS"
                         alt=""
                         class="img-fluid"
                         width="250px"
                         height="auto"
+                        style="display: block; margin: auto"
                     />
                 </div>
 
                 <div class="email-body">
-                    <h1>Poznaj nową ikonę Legnicy</h1>
+                    <h1 style="font-size: 56px">Poznaj nową ikonę Legnicy</h1>
                     <img
-                        src="https://bielanypark.pl/images/building/back.jpg"
+                        src="http://drive.google.com/uc?export=view&id=1QFea0O49AAH3GP2p1C31gAI4d7PWw71l"
                         alt=""
                         class="img-building"
                         width="900px"
                         height="auto"
+                        style="display: block; margin: auto"
                     />
                 </div>
-                <div class="container mt-5 pt-5 mb-2" style="margin: auto">
-                    <div class="row">
-                        <div class="col-md-3 text-left">
-                            <h3>KAMERALNY I LUKSUSOWY</h3>
-                            <h2>
+                <div
+                    class=""
+                    style="margin: auto; margin-top: 50px; margin-bottom: 25px"
+                >
+                    <div
+                        class=""
+                        style="
+                            display: grid;
+                            grid-template-columns: auto auto auto;
+                            margin: auto;
+                            padding-left: 40%;
+                            padding-right: 40%;
+                        "
+                    >
+                        <div class="" style="text-align: left">
+                            <h3 style="font-size: 12px">
+                                KAMERALNY I LUKSUSOWY
+                            </h3>
+                            <h2 style="font-size: 30px">
                                 Zapoznaj się <br />z premierową <br />inwestycją
                             </h2>
                         </div>
-                        <div class="col-md-5 text-left font-secondary">
+                        <div
+                            class=""
+                            style="
+                                font-family: Poppins;
+                                font-weight: 300;
+                                font-size: 16px;
+                                text-align: left;
+                            "
+                        >
                             <ul>
                                 <li>15 lokali do wynajęcia</li>
                                 <li>od 30 do 86m²</li>
@@ -112,19 +86,35 @@
                                 <li>Winda przy głównym wejściu</li>
                             </ul>
                         </div>
-                        <div class="col-md-3">
+                        <div class="">
                             <img
-                                src="https://bielanypark.pl/images/building/side_close.jpg"
+                                src="http://drive.google.com/uc?export=view&id=1l010DHZzC6Qpr64xAL3Ql02awQyfAIKI"
                                 alt=""
                                 class="img-building2"
                                 width="280px"
                                 height="auto"
+                                style="display: block; float: right"
                             />
                         </div>
                     </div>
                 </div>
-                <div class="email-footer mt-5">
-                    <h4>
+                <div
+                    class="email-footer mt-5"
+                    style="
+                        background-color: #36393b;
+                        color: #f8f3f1;
+                        font-family: Poppins;
+                        padding: 25px;
+                    "
+                >
+                    <h4
+                        style="
+                            font-size: 26px;
+                            font-family: Poppins;
+                            font-weight: 700;
+                            color: #f8f3f1;
+                        "
+                    >
                         Sprzedaż już się zaczęła, nie przegap<br />
                         okazji i skontaktuj się z nami!
                     </h4>
@@ -134,6 +124,7 @@
                             href="bielanypark.pl"
                             target="_blank"
                             rel="noopener noreferrer"
+                            style="text-decoration: underline"
                             >bielanypark.pl</a
                         >
                     </p>
@@ -142,3 +133,5 @@
         </div>
     </body>
 </html>
+
+<!-- http://drive.google.com/uc?export=view&id=1QFea0O49AAH3GP2p1C31gAI4d7PWw71l -->
