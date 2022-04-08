8dd9050ecdc3415084dc77df3a9ae8d56aaf66cc
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-08 21:47:51 +0200

    js dev

---
+++ public/js/mieszkania.js
@@ -0,0 +1,43 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./resources/js/mieszkania.js ***!
+  \************************************/
+var children = document.querySelector('#app').children;
+var i = 0;
+children[i].classList.add('selected'); // Item default selection
+
+document.getElementById("previous").addEventListener("click", previous);
+document.getElementById("next").addEventListener("click", next);
+
+function resetClass() {
+  for (var j = 0; j < children.length; j++) {
+    children[j].classList.add('d-none');
+  }
+}
+
+function next() {
+  // console.log("next");
+  if (i >= children.length - 1) {
+    return false;
+  }
+
+  resetClass();
+  i++;
+  children[i].classList.add('d-block');
+  children[i].classList.remove('d-none');
+}
+
+function previous() {
+  // console.log("prev");
+  if (i <= 0) {
+    return false;
+  }
+
+  resetClass();
+  i--;
+  children[i].classList.add('d-block');
+  children[i].classList.remove('d-none');
+}
+/******/ })()
+;
