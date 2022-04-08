642684d4d77c9cd2112a13173a4a60a49383d017
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-08 21:48:54 +0200

    controllers

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -11,4 +11,28 @@ class AppController extends Controller
             //parameters
         ]);
     }
+
+    public function kontakt(){
+        return view('kontakt', [
+            //parameters
+        ]);
+    }
+
+    public function mieszkania(){
+        return view('mieszkania', [
+            //parameters
+        ]);
+    }
+
+    public function wybor3d(){
+        return view('wybor3d', [
+            //parameters
+        ]);
+    }
+
+    public function polityka(){
+        return view('polityka', [
+            //parameters
+        ]);
+    }
 }
---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class EmailController extends Controller
+{
+    public function create()
+    {
+        return view('kontakt');
+    }
+
+    public function sendEmail(Request $request)
+    {
+        $request->validate([
+          'email' => 'required|email',
+          'name' => 'required',
+          'content' => 'required',
+          'controlInfo' => ''
+        ]);
+
+        $data = [
+          'name' => $request->name,
+          'email' => $request->email,
+          'content' => $request->content,
+          'controlInfo' => $request->controlInfo,
+          // 'logo' => asset('images/logos/logo_black.svg')
+        ];
+
+        Mail::send('email.email-template', $data, function($message) use ($data) {
+          $message->to(env('MAIL_USERNAME'))
+          ->subject("Nowa wiadomość ze strony bielanypark")
+          ->from(env('MAIL_USERNAME'), "Bielanypark")
+          ->sender(env('MAIL_USERNAME'), "Bielanypark");
+        });
+
+        Mail::send('email.email-template-user-confirm', $data, function($message) use ($data) {
+          $message->to($data['email'])
+          ->subject("Dziękujemy za kontakt!")
+          ->from(env('MAIL_USERNAME'), "Bielanypark")
+          ->sender(env('MAIL_USERNAME'), "Bielanypark");
+        });
+
+        return view('email.email-sent');
+    }
+}
