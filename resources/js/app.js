5386c9656e4c393fc8ea7da7d5f04a46c78a12de
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-12 20:46:54 +0200

    swup js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+// import { Swup } from "swup";
+import * as Swup from '/../node_modules/swup/dist/swup.min';
+import SwupHeadPlugin from '@swup/head-plugin';
+
+const swup = new Swup({
+    plugins: [new SwupHeadPlugin()]
+});
