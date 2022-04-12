bd90d8f0beda55e73a97df3105a7b7013d8ad544
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-12 17:43:59 +0200

    component MainFooter & WebStillInDev

---
+++ resources/views/components/web-still-in-dev.blade.php
@@ -0,0 +1,5 @@
+<div class="container text-center vh-100 display-grid-center">
+    <h1 class="font-color-fourth font-family-primary">Podstrona w budowie</h1>
+
+    <h2 class="underline">Zapraszamy do kontaktu</h2>
+</div>
