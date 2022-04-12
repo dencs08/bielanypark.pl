e4121a1814602d2b7f41f3a994f21b9afacf3506
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-12 17:44:08 +0200

    footer component change in start

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -104,36 +104,6 @@
     </div>
 </section>
 
-<section id="footer">
-    <div class="container text-center">
-        <img src="{{ asset('images/logos/Logo_white.svg') }}" class="img-fluid" alt="">
-
-        <div>
-            <h4 class="mb-5 font-color-fourth">GDZIE TERAZ?</h4>
-
-            <div class="font-family-secondary font-color-secondary mb-10">
-                <a href="/start" class="mx-5">Start</a>
-                <a href="/wybor3d" class="mx-5">Wybór 3D</a>
-                <a href="/mieszkania" class="mx-5">Mieszkania</a>
-                <a href="/kontakt" class="mx-5">Kontakt</a>
-            </div>
-        </div>
-
-        <div class="footer_info font-family-secondary font-color-fourth fw-light mb-5">
-            <div>biuro@komb.pl</div> 
-            <div>734 848 057</div> 
-            <div>Legnica, Plac Wolności 4AB/101</div>
-        </div>
-
-        <div>
-            <p class="footer_copy font-family-secondary font-color-sixth fw-light mb-5">Przedstawiona oferta cenowa ma charakter informacyjny, nie stanowi oferty handlowej w rozumieniu Art. 66 par. 1 Kodeksu Cywilnego oraz innych przepisów prawnych. Przedstawione na niniejszej stronie wizualizacje mają charakter wyłącznie poglądowy i stanowią wyłącznie materiał pomocniczy ułatwiający przyszłym nabywcą mieszkań zorientowanie się w ogólnym wyglądzie realizowanego przedsięwzięcia deweloperskiego. Przedstawione wizualizacje nie stanowią oferty w rozumieniu ustawy z dnia 23 kwietnia 1964 r. Kodeks cywilny (Dz. U. z 2014 r., poz. 121; ze zm.) i nie są wiążące dla Dewelopera.
-            </p>
-        </div>
-
-        <div class="footer_copy font-family-secondary font-color-sixth fw-light">
-            <p>© 2022 bielanypark wszelkie prawa zastrzeżone. Zrealizował: <a href="{{ env('APP_CREATOR') }}" rel="noreferrer" target="_blank">bisonstudio.pl</a></p>
-        </div>
-    </div>
-</section>
+<x-MainFooterDark/>
 
 @endsection
