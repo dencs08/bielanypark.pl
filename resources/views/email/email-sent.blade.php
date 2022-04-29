501ad0794250b5a62b64885f680b6b688a9de4ac
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-29 09:41:16 +0200

    made email mail sent blade more responsive

--- resources/views/email/email-sent.blade.php
+++ resources/views/email/email-sent.blade.php
@@ -14,10 +14,11 @@
             <h1>Udało się! Mail został wysłany.</h1>
             <h3>Dziękujemy za kontakt, w oczekiwaniu na odpowiedź możesz wrócić na stronę główną</h3>
             <div>
-                <a href="/start" class="underline">Strona Główna </a><x-arrow/>
+                <a href="/start" class="underline">Strona Główna </a>
+                <x-arrow/>
             </div>
             <span class="bg_text" data-scroll data-scroll-speed="3">Email wysłany</span>
-            <span class="bg_text" data-scroll data-scroll-speed="12">Dziękujemy</span>
+            <span class="bg_text sm-d-none" data-scroll data-scroll-speed="12">Dziękujemy</span>
         </div>
     </div>
 <x-MainFooterDark/>
