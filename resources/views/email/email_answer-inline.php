5f4b0e3f8199ca09afb3856e8541da1a5b014ebb
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-04-29 09:40:50 +0200

    email answer blade

---
+++ resources/views/email/email_answer-inline.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dziękujemy za zainteresowanie naszą inwestycją BIELANYPARK</title>
+    <style type="text/css">
+      @import url("https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,700;1,500&family=Poppins:wght@300&display=swap");
+    </style>
+  </head>
+  <body style="margin:0;margin: 0; padding: 0; min-width: 100%;">
+    <center class="wrapper" style="width:100%;table-layout:fixed;background-color:#fefbfa;padding-bottom:60px;">
+      <table align="center" role="presentation" class="main" width="100%" style="border-spacing:0;width:100%;max-width:600px;background-color:#f8f3f1;font-family:sans-serif;color:#36393b;box-shadow:0 0 25px rgba(0, 0, 0, 0.15);
+                    border-spacing: 0;
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    max-width: 600px;
+                ;">
+        <!-- BORDER -->
+        <tr>
+          <td height="22" class="wrong-email" style="padding:0;text-align:center;background-color: #36393b;">
+          </td>
+        </tr>
+        <!-- LOGO -->
+        <tr>
+          <td style="padding:0;padding: 14px 0 4px;">
+            <table role="presentation" width="100%" style="border-spacing:0;">
+              <tr>
+                <td class="column_container" style="padding:0;">
+                  <table role="presentation" class="column" style="border-spacing:0;width:100%;max-width:300px;text-align:center;margin:auto;margin-top:20px;">
+                    <tr>
+                      <td style="padding:0;">
+                        <h2 class="font-family-cormorant" style='font-size:32px;font-weight:600;line-height:32px;margin:8px;font-family:"Cormorant", serif;'>Witam,</h2>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- HERO -->
+        <tr>
+          <td class="hero" style="padding:0;text-align:center;padding-bottom:50px;">
+            <table role="presentation" width="100%" style="border-spacing:0;">
+              <tr>
+                <td style="padding:0;">
+                  <h1 class="font-family-poppins" style='font-size:20px;font-weight:600;margin:0 30px 15px 30px;font-family:"Poppins", sans-serif;'>
+                    Cieszę się, że zainteresował/a się Pan/i naszym nowym projektem BIELANY PARK. Doskonała lokalizacja oraz wysoka jakość wykonania to jedne z największych atutów tej inwestycji. Zakończenie realizacji projektu zaplanowaliśmy na  czerwiec 2023. 
+                                    </h1>
+                  <a href="https://bielanypark.pl/"><img src="https://www.dropbox.com/s/0gtwye8p0iiaux2/back.jpg?raw=1" alt="zdjęcie budynku wieczorem" width="900px" style="border:0;margin-bottom:20px;max-width: 100%;" class="hero_img"></a>
+                </td>
+              </tr>
+              <tr>
+                <td class="font-family-poppins" style='padding:0;font-family:"Poppins", sans-serif;'>
+                  <span>W całym budynku zaprojektowaliśmy tylko lokale użytkowe.</span>
+                  <br>
+                  <span>Więcej informacji </span><a href="https://www.bielanypark.pl/">tutaj.</a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- Price -->
+        <tr>
+          <td class="hero" style="padding:0;text-align:center;padding-bottom:50px;">
+            <table role="presentation" width="100%" style="border-spacing:0;">
+              <tr>
+                <td class="font-family-poppins" style='padding:0;font-family:"Poppins", sans-serif;'>
+                  <table style="border-spacing:0;">
+                    <h3 style="font-size:18px;font-weight:600;margin:0;margin-bottom:15px;">
+                      Cena lokalu, którym jest Pan/i zainteresowany/a:
+                                            </h3>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding:0;text-align:center; margin:auto;">
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- MAP -->
+        <tr>
+          <td class="hero" style="padding:0;text-align:center;padding-bottom:50px;">
+            <table role="presentation" width="100%" style="border-spacing:0;">
+              <tr>
+                <td class="font-family-poppins" style='padding:0;font-family:"Poppins", sans-serif;'>
+                  <h3 style="font-size:18px;font-weight:600;margin:0;">
+                    Lokalizacja działki 12/15 Bielany:
+                                    </h3>
+                  <a href="https://www.dropbox.com/s/nt3sywb5de1cti1/bielany_map.jpg?raw=1"><img src="https://www.dropbox.com/s/nt3sywb5de1cti1/bielany_map.jpg?raw=1" alt="mapa lokalizacji inwestycji bielanypark" width="900px" style="border:0;max-width: 100%; margin-top: 10px;"></a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- Attachments -->
+        <tr>
+          <td class="hero" style="padding:0;text-align:center;padding-bottom:50px;">
+            <table role="presentation" width="100%" style="border-spacing:0;">
+              <tr>
+                <td class="font-family-poppins" style='padding:0;font-family:"Poppins", sans-serif;'>
+                  <h3 style="font-size:18px;font-weight:600;margin:0;font-weight:300; font-size: 16px;">
+                    Dodatkowo załączam:
+                                    </h3>
+                  <p style="font-size:14px; font-style: italic;">
+                    ZAŁĄCZNIKI DO UZUPEŁNIENIA
+                                    </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- FOOTER SECTION -->
+        <tr>
+          <td style="padding:0;background-color: #36393b; padding: 25px;">
+            <table role="presentation" width="100%" class="footer" style="border-spacing:0;color:#f8f3f1;text-align:center;">
+              <tr>
+                <td style="padding:0;">
+                  <h4 style="font-size:24px;">
+                    Zapraszamy do biura sprzedaży, zlokalizowanego przy ul.
+                                        Plac Wolności 4AB/101
+                                    </h4>
+                  <p>
+                    Więcej na
+                                        <a href="https://bielanypark.pl/" style="color:cornflowerblue;text-decoration:none;">bielanypark.pl</a>
+                  </p>
+                  <p>
+                    Inwestor
+                                        <a href="http://komb.pl/" style="color:cornflowerblue;text-decoration:none;">komb.pl</a>
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- BORDER -->
+        <tr>
+          <td height="5" style="padding:0;background-color: black;"></td>
+        </tr>
+      </table>
+      <!-- End Main Class -->
+    </center>
+    <!-- End Wrapper -->
+  </body>
+</html>
---
+++ resources/views/email/email_answer.blade.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Dziękujemy za zainteresowanie naszą inwestycją BIELANYPARK</title>
+        <style type="text/css">
+            @import url("https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,700;1,500&family=Poppins:wght@300&display=swap");
+            body {
+                margin: 0;
+            }
+            table {
+                border-spacing: 0;
+                /* border: 1px solid #111; */
+            }
+            td {
+                padding: 0;
+            }
+            img {
+                border: 0;
+            }
+            .wrapper {
+                width: 100%;
+                table-layout: fixed;
+                background-color: #fefbfa;
+                padding-bottom: 60px;
+            }
+            .main {
+                width: 100%;
+                max-width: 600px;
+                background-color: #f8f3f1;
+                font-family: sans-serif;
+                color: #36393b;
+                box-shadow: 0 0 25px rgba(0, 0, 0, 0.15);
+            }
+            .column_container .column {
+                width: 100%;
+                max-width: 300px;
+                text-align: center;
+                margin: auto;
+                margin-top: 20px;
+            }
+            .hero {
+                text-align: center;
+                padding-bottom: 50px;
+            }
+            h1 {
+                font-size: 20px;
+                font-weight: 600;
+                margin: 0 30px 15px 30px;
+            }
+            h2 {
+
+                font-size: 32px;
+                font-weight: 600;
+                line-height: 32px;
+                margin: 8px;
+            }
+            h3 {
+                font-size: 18px;
+                font-weight: 600;
+                margin: 0;
+            }
+
+            .two-columns-hero {
+                text-align: center;
+            }
+
+            .two-columns {
+                font-size: 0;
+                padding: 35px 0 20px;
+                text-align: center;
+            }
+
+            .two-columns .column {
+                width: 100%;
+                max-width: 300px;
+                display: inline-block;
+                vertical-align: top;
+                text-align: start;
+            }
+
+            .two-columns .padding {
+                padding: 10px;
+            }
+
+            .two-columns .content {
+                font-size: 15px;
+                line-height: 20px;
+            }
+            .footer {
+                color: #f8f3f1;
+                text-align: center;
+            }
+            .footer a {
+                color: cornflowerblue;
+                text-decoration: none;
+            }
+            h4 {
+                font-size: 24px;
+            }
+            .wrong-email {
+                text-align: center;
+            }
+            .wrong-email span {
+                font-size: 12px;
+                text-decoration: none;
+                color: #f8f3f1;
+            }
+            .wrong-email a {
+                font-size: 12px;
+                text-decoration: underline;
+                color: cornflowerblue;
+            }
+
+            .hero_img{
+                margin-bottom: 20px;
+            }
+
+            .font-family-poppins{
+                font-family: "Poppins", sans-serif;
+            }
+
+            .font-family-cormorant{
+                font-family: "Cormorant", serif;
+            }
+        </style>
+    </head>
+    <body style="margin: 0; padding: 0; min-width: 100%">
+
+        <center class="wrapper">
+            <table
+                align="center"
+                role="presentation"
+                class="main"
+                width="100%"
+                style="
+                    border-spacing: 0;
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    max-width: 600px;
+                "
+            >
+                <!-- BORDER -->
+                <tr>
+                    <td
+                        height="22"
+                        class="wrong-email"
+                        style="background-color: #36393b"
+                    >
+                    </td>
+                </tr>
+                <!-- LOGO -->
+                <tr>
+                    <td style="padding: 14px 0 4px">
+                        <table role="presentation" width="100%">
+                            <tr>
+                                <td class="column_container">
+                                    <table role="presentation" class="column">
+                                        <tr>
+                                            <td>
+                                                <h2 class="font-family-cormorant">Witam,</h2>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- HERO -->
+                <tr>
+                    <td class="hero">
+                        <table role="presentation" width="100%">
+                            <tr>
+                                <td>
+                                    <h1 class="font-family-poppins">
+                                    Cieszę się, że zainteresował/a się Pan/i naszym nowym projektem BIELANY PARK. Doskonała lokalizacja oraz wysoka jakość wykonania to jedne z największych atutów tej inwestycji. Zakończenie realizacji projektu zaplanowaliśmy na  czerwiec 2023. 
+                                    </h1>
+                                    <a href="https://bielanypark.pl/"
+                                        ><img
+                                            src="https://www.dropbox.com/s/0gtwye8p0iiaux2/back.jpg?raw=1"
+                                            alt="zdjęcie budynku wieczorem"
+                                            width="900px"
+                                            style="max-width: 100%"
+                                            class="hero_img"
+                                    /></a>
+
+                                    <tr>
+                                        <td class="font-family-poppins">
+                                            <span >W całym budynku zaprojektowaliśmy tylko lokale użytkowe.</span>
+                                            <br>
+                                            <span>Więcej informacji </span><a href="https://www.bielanypark.pl/">tutaj.</a>
+                                        </td>
+                                    </tr>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+                <!-- Price -->
+                <tr>
+                    <td class="hero">
+                        <table role="presentation" width="100%">
+                            <tr>
+                                <td class="font-family-poppins">
+                                        <table>
+                                            <h3 style="margin-bottom:15px;">
+                                            Cena lokalu, którym jest Pan/i zainteresowany/a:
+                                            </h3>
+                                        </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td  align="center" style="text-align:center; margin:auto;">
+
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+                <!-- MAP -->
+                <tr>
+                    <td class="hero">
+                        <table role="presentation" width="100%">
+                            <tr>
+                                <td class="font-family-poppins">
+                                    <h3>
+                                        Lokalizacja działki 12/15 Bielany:
+                                    </h3>
+                                    <a href="https://www.dropbox.com/s/nt3sywb5de1cti1/bielany_map.jpg?raw=1"
+                                        ><img
+                                            src="https://www.dropbox.com/s/nt3sywb5de1cti1/bielany_map.jpg?raw=1"
+                                            alt="mapa lokalizacji inwestycji bielanypark"
+                                            width="900px"
+                                            style="max-width: 100%; margin-top: 10px;"
+                                    /></a>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+                <!-- Attachments -->
+                <tr>
+                    <td class="hero">
+                        <table role="presentation" width="100%">
+                            <tr>
+                                <td class="font-family-poppins">
+                                    <h3  style="font-weight:300; font-size: 16px;">
+                                        Dodatkowo załączam:
+                                    </h3>
+                                    <p style="font-size:14px; font-style: italic;">
+                                        ZAŁĄCZNIKI DO UZUPEŁNIENIA
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+
+                <!-- FOOTER SECTION -->
+                <tr>
+                    <td style="background-color: #36393b; padding: 25px">
+                        <table role="presentation" width="100%" class="footer">
+                            <tr>
+                                <td>
+                                    <h4>
+                                        Zapraszamy do biura sprzedaży, zlokalizowanego przy ul.
+                                        Plac Wolności 4AB/101
+                                    </h4>
+                                    <p>
+                                        Więcej na
+                                        <a href="https://bielanypark.pl/"
+                                            >bielanypark.pl</a
+                                        >
+                                    </p>
+
+                                    <p>
+                                        Inwestor
+                                        <a href="http://komb.pl/"
+                                            >komb.pl</a
+                                        >
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- BORDER -->
+                <tr>
+                    <td height="5" style="background-color: black"></td>
+                </tr>
+            </table>
+            <!-- End Main Class -->
+        </center>
+        <!-- End Wrapper -->
+    </body>
+</html>
