f7d8a87094d24d38bd4394e22973a76c81fc1575
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-05-01 14:17:21 +0200

    storefront migration added

---
+++ database/migrations/2022_05_01_120606_create_storefront_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storefront', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('metric');
+            $table->int('floor');
+            $table->string('sanitary');
+            $table->boolean('available');
+            $table->float('buyPrice');
+            $table->float('rentPrice');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storefront');
+    }
+};
