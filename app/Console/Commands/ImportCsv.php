f383833d77ffed91d027c4b9dbe71833e5ec0452
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-05-01 23:17:15 +0200

    changed migration inserting to a command

---
+++ app/Console/Commands/ImportCsv.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Storefront;
+use Illuminate\Support\Facades\DB;
+
+class ImportCsv extends Command
+{
+    protected $signature = 'CSV:import';
+    protected $description = 'none';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $file = dirname(__DIR__, 3).'/CSV/bielanypark_data.csv';
+        $delimiter = ',';
+        
+        if (!file_exists($file) || !is_readable($file))
+        dd('file doesnt exists or is not readable'); 
+        
+        $header = null;
+        $data = array();
+        if (($handle = fopen($file, 'r')) !== false)
+        {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
+            {
+                if (!$header)
+                $header = $row;
+                else
+                $data[] = array_combine($header, $row);
+            }
+            fclose($handle);
+        }
+
+        $arr = $data;
+        for ($i = 0; $i < count($arr); $i ++)
+        {
+            DB::table('storefront')->insert([
+                $arr[$i]
+            ]);
+        }
+
+         dump('CSV import success!'); 
+    }
+}
