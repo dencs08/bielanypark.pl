85ca4958e14c85ff7953f333ccb250dc5f11fb7a
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-05-08 15:09:40 +0200

    web route update with filter routes

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,20 @@ Route::get('email_answer', function () {
 
 Route::get('/', [App\Http\Controllers\AppController::class, 'start']);
 Route::get('start', [App\Http\Controllers\AppController::class, 'start']);
-Route::get('wybor3d', [App\Http\Controllers\AppController::class, 'wybor3d']);
 Route::get('polityka', [App\Http\Controllers\AppController::class, 'polityka']);
 
+// Route::get('wybor3d', [App\Http\Controllers\AppController::class, 'wybor3d']);
 // Route::get('kontakt', [App\Http\Controllers\AppController::class, 'kontakt']);
 Route::get('/kontakt', [App\Http\Controllers\EmailController::class, 'create']);
+Route::get('/kontakt/{name}', [App\Http\Controllers\EmailController::class, 'createAsk']);
 Route::post('/kontaktsent', [App\Http\Controllers\EmailController::class, 'sendEmail'])->name('send.email');
 
 Route::get('lokale', [App\Http\Controllers\AppController::class, 'lokale']);
 Route::get('lokale/lokal', [App\Http\Controllers\StorefrontController::class, 'index']);
+
+Route::get('lokale/{name}', [App\Http\Controllers\AppController::class, 'lokalID']);
+
+// Route::get('testFilter/{id}', [App\Http\Controllers\FilterController::class, 'testFilter']);
+// Route::get('testFilter/{id}', [App\Http\Controllers\StorefrontController::class, 'index']);
+
+Route::get('testFilter/', [App\Http\Controllers\StorefrontController::class, 'index']);
