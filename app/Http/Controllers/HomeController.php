c4a2eb5a191b0b296db756afe0747de30b08fdd2
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-05-27 15:27:52 +0200

    add: Auth controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Storefront;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $request = new Request;
+        $results = Storefront::all();
+
+
+        
+        // $floors_decoded = json_decode($floors, false);
+        return view('home', [
+            'results' => $results,
+        ]);
+    }
+}
