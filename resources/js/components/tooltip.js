f702a442b65f1e60ac1ff33a81fd89bdccef8ab0
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-02 22:23:12 +0200

    add: tooltip js status

--- resources/js/components/tooltip.js
+++ resources/js/components/tooltip.js
@@ -27,6 +27,8 @@ function tooltipShow() {
     let tooltipMetric
     let tooltipPrice
     let tooltipFloor
+    let tooltipAvailable
+    let available
 
     app.addEventListener('mouseout', e => {
         if (!e.target.matches("[data-tooltip]")) return
@@ -38,16 +40,19 @@ function tooltipShow() {
         name = e.target.getAttribute('data-tooltip-name')
         metric = e.target.getAttribute('data-tooltip-metric')
         floor = e.target.getAttribute('data-tooltip-floor')
+        available = e.target.getAttribute('data-tooltip-available')
         // buyPrice = e.target.getAttribute('data-tooltip-buy-price')
 
         tooltipName = tooltip.querySelector("[data-tooltip-name]")
         tooltipMetric = tooltip.querySelector("[data-tooltip-metric]")
         tooltipFloor = tooltip.querySelector("[data-tooltip-floor]")
+        tooltipAvailable = tooltip.querySelector("[data-tooltip-available]")
         // tooltipPrice = tooltip.querySelector("[data-tooltip-price]")
 
         tooltipName.innerHTML = "Lokal " + name
         tooltipMetric.innerHTML = metric + "m²"
         tooltipFloor.innerHTML = floor
+        tooltipAvailable.innerHTML = available
         // tooltipPrice.innerHTML = buyPrice + "zł"
     });
     app.addEventListener('mousemove', e => {
