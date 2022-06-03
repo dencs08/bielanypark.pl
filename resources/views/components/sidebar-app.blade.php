5dc5b106c76236b95c7736ce8dd27b9e546acce4
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-06-03 20:47:44 +0200

    update: removed redundant code from sidebar

--- resources/views/components/sidebar-app.blade.php
+++ resources/views/components/sidebar-app.blade.php
@@ -2,15 +2,11 @@
     <button class="btn btn-primary mb-2" data-floor-picker="main" >Wróć</button>
     <div id="tooltip" data-tooltip-object>
         <div class="row">
-            <!-- <div class="col-md-3">
-                <button class="btn btn-floor mb-2" data-floor-picker="0" >Piętro 0</button>
-                <button class="btn btn-floor mb-2" data-floor-picker="1" >Piętro 1</button>
-            </div> -->
             <div class="col-md-12">
                 <h4 data-tooltip-name>Lokal</h4>
-                <p>Metraż: <span data-tooltip-metric>1</span></p>
-                <p>Piętro: <span data-tooltip-floor>2</span></p>
-                <p>Status: <span data-tooltip-available>Dostępne</span></p>
+                <p>Metraż: <span data-tooltip-metric></span></p>
+                <p>Piętro: <span data-tooltip-floor></span></p>
+                <p>Status: <span data-tooltip-available></span></p>
             </div>
         </div>
     </div>
